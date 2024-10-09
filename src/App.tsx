cdac124a8209a623570915a3a2dd77e697f8cf35
coding <coding@MacBook-Pro-de-coding.local>
2024-10-09 13:30:36 +0200

    add:input-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Navigation from './components/Navigation/Navigation';
+// import InputField from './components/Input/Input';
+// import FormGroup from './components/Input/FormGroup';
+import InputPage from './pages/InputPage';
 
 function App() {
 
-  	return (
-		<BrowserRouter>
-			<div className="container">
-				<Navigation />
-				<main className="content">
-					<Routes>
-						{/* <Route path="/" element={} /> */}
-					</Routes>
-				</main>
-			</div>
-		</BrowserRouter>
-  	);
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Navigation />
+        <main className="content">
+          <Routes>
+            <Route path="/input" element={<InputPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
