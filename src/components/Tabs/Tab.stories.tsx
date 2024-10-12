bea3cf4179815c0aac557f115fd938d679ea9f32
lennyCoding <lenny.xiong@edu.esiee-it.fr>
2024-10-12 21:31:04 +0200

    fix storybook

--- src/components/Tabs/Tab.stories.tsx
+++ src/components/Tabs/Tab.stories.tsx
@@ -47,6 +47,32 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {},
+export const ThreeTabs: Story = {
+  args: {
+    list: ["tab1", "tab2", "tab3"],
+    defaultId: "tab1",
+    children: (activeTab) => {
+      return (
+        <>
+          <TabPanel id="tab1" activeTab={activeTab}>
+            tab1
+          </TabPanel>
+          <TabPanel id="tab2" activeTab={activeTab}>
+            <img src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" />
+          </TabPanel>
+          <TabPanel id="tab3" activeTab={activeTab}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            pharetra aliquet ultricies. In nec ante tellus. Vivamus interdum
+            risus dolor, sed blandit justo porttitor et. Fusce et risus
+            facilisis, facilisis diam ac, cursus nisl. Vestibulum eget suscipit
+            urna, id euismod purus. Nam ex velit, iaculis at felis non, mattis
+            dictum elit. Quisque scelerisque ex at feugiat efficitur. Nam
+            facilisis blandit turpis ut sollicitudin. Nulla vehicula est vitae
+            metus consectetur viverra. Maecenas tempor convallis tortor vel
+            scelerisque.
+          </TabPanel>
+        </>
+      );
+    },
+  },
 };
